4bb163b8cbc0c7570e99eadad0695f3545db5d3e
Jadon <jiadong001@e.ntu.edu.sg>
2022-09-14 20:47:42 +0800

    [data] Config tn data generation to more tool types

--- prepare_tn_dataset.py
+++ prepare_tn_dataset.py
@@ -17,7 +17,7 @@ import ray
 from os.path import exists
 import matplotlib.pyplot as plt
 
-def generate_tn_object(obj_path, obj_bounds, voxel_size, image_size, target_height):
+def generate_tn_object(obj_path, output_path, voxel_size, image_size, target_height, target_length):
     """ Generate object for TransporterNet
     """
 
@@ -35,9 +35,12 @@ def generate_tn_object(obj_path, obj_bounds, voxel_size, image_size, target_heig
     camera = SimCameraBase(view_matrix, image_size,
                            z_near=-z-0.05, z_far=-z+0.05, focal_length=focal_length)
 
+    # load and modify meshes
     mesh = trimesh.load(obj_path, force='mesh')
-    scale_factor = 0.25 / (mesh.vertices.max(axis=0)-mesh.vertices.min(axis=0))[1]
+    # scale to suitable size
+    scale_factor = target_length / (mesh.vertices.max(axis=0)-mesh.vertices.min(axis=0))[1]
     mesh.vertices *= scale_factor
+    mesh.vertices[:, 1] -= ( (mesh.vertices.max(axis=0) + mesh.vertices.min(axis=0)) / 2)[1]
     mesh.vertices[:, 2] -= mesh.vertices.min(axis=0)[2] + 0
     processed_obj_path = obj_path.parent / f"obj.obj"
     mesh.export(processed_obj_path)
@@ -61,6 +64,7 @@ def generate_tn_object(obj_path, obj_bounds, voxel_size, image_size, target_heig
     obj_bounds_zoomed = np.copy(obj_bounds)
     obj_bounds_zoomed[:2, 0] -= delta  # Only along x,y axis
     obj_bounds_zoomed[:2, 1] += delta
+    # obj_bounds_zoomed = obj_bounds
 
     print(obj_bounds)
     part_tsdf = TSDFHelper.tsdf_from_camera_data(
@@ -82,13 +86,14 @@ def generate_tn_object(obj_path, obj_bounds, voxel_size, image_size, target_heig
     # plt.show()
 
     height = target_height
-    occ_grid = np.repeat(occ_grid_proj[:,:,np.newaxis], int(height/voxel_size), axis=2)
+    layer = int(target_height / voxel_size) + 2
+    occ_grid = np.repeat(occ_grid_proj[:,:,np.newaxis], layer, axis=2)
     # occ_grid[occ_grid == 0] = 1
     # occ_grid = -occ_grid
 
-    tn_obj_path = obj_path.parent / "tn_obj.obj"
-    if TSDFHelper.to_mesh(occ_grid, tn_obj_path, voxel_size, vol_origin=[0, 0, height/2]):
-        print(tn_obj_path)
+    # tn_obj_path = obj_path.parent / "tn_obj.obj"
+    if TSDFHelper.to_mesh(occ_grid, output_path, voxel_size, vol_origin=[0, 0, (layer - 1) / 2.0 * voxel_size]):
+        print(output_path)
     else:
         print("Failed")
 
@@ -97,32 +102,60 @@ def generate_tn_object(obj_path, obj_bounds, voxel_size, image_size, target_heig
 @hydra.main(config_path="conf/data_gen", config_name="tn")
 def main(cfg: DictConfig):
 
-    tool_size = {"hammer":15}
-
     def get_obj_paths(data_path, tool_size):
         """Find the folder path of object"""
-        obj_paths = []
-        for tool, size in tool_size.items():
-            data_path = data_path / tool
-            for i in range(size):
-                obj_path = data_path / str(i)
+        obj_paths = dict()
+        for tool, num in tool_size.items():
+            tool_path = data_path / tool
+            obj_paths[tool] = []
+            for i in range(num):
+                obj_path = tool_path / str(i)
                 obj_path = obj_path / f"object.obj"
-                obj_paths.append(obj_path)
+                obj_paths[tool].append(obj_path)
                 print(obj_path)
         return obj_paths
+
+    # get object info
+    tool_info = cfg.tool_info
+    tool_size = {tool['type']:tool['num'] for tool in tool_info}
+                # {"hammer":15, "plier" :15}
+    # print(tool_size)
 
     # get object paths
     data_path = Path(cfg.root_data_path)
     object_paths = get_obj_paths(data_path, tool_size)
+    output_path = Path(cfg.output_data_path)
 
     # get parameters
     image_size = np.array(cfg.image_size)
     voxel_size = cfg.voxel_size
     target_height = cfg.target_height
 
-    for i in range(tool_size["hammer"]):
-        generate_tn_object(object_paths[i], None, voxel_size, image_size, target_height)
-    return
+    # for tool, num in tool_size.items():
+    for tool in tool_info:
+        type = tool['type']
+        num = tool['num']
+        target_length = tool['length']
+
+        # create output directory
+        tool_folder = output_path / type
+        tool_folder.mkdir(parents=True, exist_ok=True)
+
+        target_length = target_length
+        for i in range(num):
+            tn_obj_path = tool_folder / f'{i:02d}.obj'
+
+            generate_tn_object(object_paths[type][i],tn_obj_path, voxel_size,
+                               image_size, target_height, target_length)
+            # break
+
+        # for i in range(num):
+
+    # output_path = Path(cfg.output_data_path)
+    # sub_output_path = output_path / f"hammer" / f'{1:02d}' 
+    # sub_output_path.mkdir(parents=True, exist_ok=True)
+    # generate_tn_object(object_paths["hammer"][0], None, voxel_size, image_size, target_height, tool_info[0]['length'])
+    # return
 
 
 if __name__ == "__main__":
